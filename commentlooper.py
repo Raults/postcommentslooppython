493a18eadc22041cffa62f30f3b5a718a84761df
Ryan <ryan.e.tibbetts@gmail.com>
2020-06-14 00:44:06 -0400

    made facebook commenter loop, testing phases.

---
+++ commentlooper.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+x=1
+smokingStatistics = ['Here is some statistics on smoking you can read up on while I got to bed! SEE YOU LATER NERDSSSSS!!! See you on youtube XDDD!!!','https://www.cdc.gov/tobacco/campaign/tips/resources/data/cigarette-smoking-in-united-states.html?s_cid=OSH_tips_GL0005&utm_source=google&utm_medium=cpc&utm_campaign=Tips+2020%3BS%3BWL%3BBR%3BIMM%3BDTC%3BCO&utm_content=Smoking+-+Facts_E&utm_term=statistics+about+smoking&gclid=CjwKCAjw8pH3BRAXEiwA1pvMsR_8vP0dm4oihgWPOSIBJMp94ajlJg4C8sO-m7vXUJyqdENrpFmVOhoCMTYQAvD_BwE&gclsrc=aw.ds','https://www.thetruth.com/the-facts?&cid=VAP_SEARCH_GOOGLEGRANTS_NONBRAND&gclid=CjwKCAjw8pH3BRAXEiwA1pvMsRE2kASGLzWbhrz2iOfh7zhRIJClkHaWqEinParHuXcsM-v-IEDHphoCUf0QAvD_BwE&gclsrc=aw.ds','https://www.centeronaddiction.org/?gclid=CjwKCAjw8pH3BRAXEiwA1pvMsU1WjVG3Cyvfq-x4C5jdFyxJjWrreH4_aUvCAOOxlBH2pVOy2wAsxhoCTW0QAvD_BwE','https://www.cdc.gov/tobacco/data_statistics/fact_sheets/fast_facts/index.htm#:~:text=Cigarette%20smoking%20is%20responsible%20for,or%201%2C300%20deaths%20every%20day.&text=On%20average%2C%20smokers%20die%2010%20years%20earlier%20than%20nonsmokers.','https://www.cdc.gov/tobacco/data_statistics/fact_sheets/adult_data/cig_smoking/index.htm','https://www.verywellmind.com/facts-and-statistics-about-cigarette-smoking-2825328','https://ourworldindata.org/smoking','https://www.who.int/news-room/fact-sheets/detail/tobacco','https://ash.org/programs/tobacco-statistics-facts/','https://www.statista.com/topics/1600/smoking/','https://www.lung.org/research/trends-in-lung-disease/tobacco-trends-brief/overall-tobacco-trends','https://en.wikipedia.org/wiki/Prevalence_of_tobacco_use','https://www.swedish.org/classes-and-resources/smoking-cessation/smoking-statistics','Like and subscribe :D https://www.youtube.com/channel/UCtdIELIZlRLJqrKp6vTa50g?view_as=subscriber']
+
+while x !=0:
+    pyautogui.scroll(-10)
+    pyautogui.click(800,700)
+    pyautogui.write('Hello world!\n')
+    time.sleep(5)
